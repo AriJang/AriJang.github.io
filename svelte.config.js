3e597ca4a9f83b57cf20aa43f845b34bbafe2c22
AriJang <eddyjang@aribio.com>
2024-09-23 13:57:19 +0900

    github page 배포를 위한 svelte config 수정

--- svelte.config.js
+++ svelte.config.js
@@ -3,11 +3,10 @@ import adapter from '@sveltejs/adapter-static';
 export default {
   kit: {
     adapter: adapter({
-      // GitHub Pages에서는 404 페이지가 필요할 수 있습니다.
-      fallback: '200.html'
+      fallback: '200.html' // SPA 라우팅을 위한 fallback 설정
     }),
     paths: {
-      base: process.env.NODE_ENV === 'production' ? '/<repository-name>' : ''
+      base: '' // base 경로를 비워둡니다.
     }
   }
 };
