4cd7da6499b2e580acc80801d669c786b82a9d8c
AriJang <eddyjang@aribio.com>
2024-09-23 17:02:00 +0900

    정적사이트(SSG) 적용

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,15 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static'
 
 export default {
   kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: '200.html'
-    }),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/arijang.github.io' : ''
-    }
+      adapter: adapter({
+          // default options are shown. On some platforms
+          // these options are set automatically — see below
+          pages: 'build',
+          assets: 'build',
+          fallback: '200.html',
+          precompress: false,
+          strict: true
+      })
   }
 };
